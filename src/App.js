36ebf288b85a21155bb0fc85119db63e52d84f4d
badal kumar <rk765528@gmail.com>
2024-05-25 15:50:41 +0530

    baando initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the learning page of React js</h1>
+      <h1>Welcome to the learning page of React js </h1>
     </div>
   );
 }
