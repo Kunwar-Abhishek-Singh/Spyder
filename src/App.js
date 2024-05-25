ce30a8593956b7313e283cfbc4e352419c719e4c
raviraj123 <raviraj132000@gmail.com>
2024-05-25 16:11:30 +0530

    ravi grover color yellow

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>Welcome to the learning page of React js</h1>
       <h1>I am shivam singh Present</h1>
-      <h1>I am RAVI RAJ Present</h1>
+      <h1 className='ravi' >I am RAVI RAJ Present</h1>
       <h1>i am saurabh kumar pesent</h1>
       <h1>I am pritam kumar present</h1>
       <h1>I am Raushan Kumar present</h1>
